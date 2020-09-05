df80e2c34922dd99a97864dedfb530d5da6ebed6
Anshuman Aggarwal <anshuman01000001@gmail.com>
2020-09-05 10:28:10 -0400

    Update TextEditor.java

--- Code/TextEditor.java
+++ Code/TextEditor.java
@@ -1,4 +1,10 @@
-
+import java.awt.*;
+import java.awt.event.*;
+import java.io.*;
+import java.util.*;
+import javax.swing.*;
+import javax.swing.event.*;
+import javax.swing.filechooser.*;
 
 public class TextEditor extends JFrame implements ActionListener{
 	
