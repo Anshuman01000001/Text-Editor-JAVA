19b01f9a95c4939a6dd0835570675d96b9999ac2
Anshuman Aggarwal <anshuman01000001@gmail.com>
2020-09-11 20:01:57 -0400

    Update TextEditor.java

--- Code/TextEditor.java
+++ Code/TextEditor.java
@@ -8,12 +8,7 @@ import javax.swing.filechooser.*;
 
 public class TextEditor extends JFrame implements ActionListener{
 	
-	JTextArea textArea;
-	JScrollPane scrollPane;
-	JLabel fontLabel;
-	JSpinner fontSizeSpinner;
-	JButton fontColorButton;
-	JComboBox fontBox;
+	
  
 	JMenuBar menuBar;
 	JMenu fileMenu;
